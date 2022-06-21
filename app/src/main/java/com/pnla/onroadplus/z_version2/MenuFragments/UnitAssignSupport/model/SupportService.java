e2ad23372c9d4bf3a254de3b3825f1a49a7962ed
francisco <newlandappscontact@gmail.com>
2022-06-21 12:31:10 -0500

    Version 8 (2.0.60) Commit antes de cambiar toda la ruta al nuevo EndPoint

--- app/src/main/java/com/pnla/onroadplus/z_version2/MenuFragments/UnitAssignSupport/model/SupportService.java
+++ app/src/main/java/com/pnla/onroadplus/z_version2/MenuFragments/UnitAssignSupport/model/SupportService.java
@@ -13,6 +13,7 @@ import retrofit2.http.POST;
 public interface SupportService {
     @POST(RetrofitEndPointsV2.GET_VEHICLES_GROUP_LIST_VEHICLES)
     Call<GroupvehicleInsideResponse> getVehiclesInGroups(@Body GroupvehicleInsideRequest request);
+    //Call<SupportUnitResponse> getVehiclesInGroups(@Body SupportUnitRequest request);
     //@POST(RetrofitEndPointsV2.GET_SUPPORTROUTES)
     //Call<SupportUnitResponse> getFullSupportUnits(@Body SupportUnitRequest request);
 }
