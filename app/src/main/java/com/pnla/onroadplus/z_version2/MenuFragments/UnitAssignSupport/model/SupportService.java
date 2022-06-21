a89d0531276eb209ae2105f0a0197a1b64b546b3
francisco <newlandappscontact@gmail.com>
2022-06-21 12:00:51 -0500

    Version 8 (2.0.60) Commit antes de cambiar toda la ruta al nuevo EndPoint

--- app/src/main/java/com/pnla/onroadplus/z_version2/MenuFragments/UnitAssignSupport/model/SupportService.java
+++ app/src/main/java/com/pnla/onroadplus/z_version2/MenuFragments/UnitAssignSupport/model/SupportService.java
@@ -2,6 +2,8 @@ package com.pnla.onroadplus.z_version2.MenuFragments.UnitAssignSupport.model;
 
 import com.pnla.onroadplus.z_version2.MenuFragments.Units.data.GroupvehicleInsideRequest;
 import com.pnla.onroadplus.z_version2.MenuFragments.Units.data.GroupvehicleInsideResponse;
+import com.pnla.onroadplus.z_version2.MenuFragments.Units.data.SupportUnitRequest;
+import com.pnla.onroadplus.z_version2.MenuFragments.Units.data.SupportUnitResponse;
 import com.pnla.onroadplus.z_version2.retrofit.RetrofitEndPointsV2;
 
 import retrofit2.Call;
@@ -9,7 +11,8 @@ import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface SupportService {
-    //@POST(RetrofitEndPointsV2.GET_VEHICLES_GROUP_LIST_VEHICLES)
-    @POST(RetrofitEndPointsV2.GET_SUPPORTROUTES)
+    @POST(RetrofitEndPointsV2.GET_VEHICLES_GROUP_LIST_VEHICLES)
     Call<GroupvehicleInsideResponse> getVehiclesInGroups(@Body GroupvehicleInsideRequest request);
+    //@POST(RetrofitEndPointsV2.GET_SUPPORTROUTES)
+    //Call<SupportUnitResponse> getFullSupportUnits(@Body SupportUnitRequest request);
 }
